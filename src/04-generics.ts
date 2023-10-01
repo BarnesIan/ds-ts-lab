35da1092c85875e5ebe12f8771ced48ded3bed91
barnesian <74927667+IanBarnes96@users.noreply.github.com>
2023-10-01 23:03:35 +0100

    Functions with generics

---
+++ src/04-generics.ts
@@ -0,0 +1,24 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './myTypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+//Sorting function to feed in array and 2 numbers to sort them via that number
+function sort<T>(array: T[], criteria: (a: T, b : T) => number): T[]{
+return array.sort(criteria);
+
+}
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
